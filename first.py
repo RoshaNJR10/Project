7dac14908ca061b529fcde3622c57eeb69d7f063
RoshaNJR10 <113534987+RoshaNJR10@users.noreply.github.com>
2023-01-01 17:20:45 +0545

    Add files via upload

---
+++ first.py
@@ -0,0 +1,65 @@
+#print simple statementt
+"""print ("hello World!!! Welcome to our python class")
+ st="abc"
+ print(st)
+ st1=123
+ print(type(st1))"""
+
+# pritnt datatype
+"""s=input("enter any number")
+ print(type(s))
+
+ t=int(input("enter any number"))
+ print(type(t))"""
+
+# if else statement
+""" s=int(input("enter an number"))
+ t=int(input("enter an number"))
+ sum=s+t
+ print (sum)
+ if s==5:
+     print("this is 5")
+ elif t==10:
+     print("this is 10")
+ else:
+     print("Sorry incorrect input")"""
+
+
+# //even or odd
+""" a=int(input("enter a number"))
+ if a%2==0:
+     print ("numbe is even")
+ else:
+     print ("the number is odd")"""
+    
+#//positiev or negative
+"""a=int(input("enter any number"))
+if a>0:
+    print("the number is positive")
+elif a==0:
+    print("The number is zero")
+else:
+    print("the numbe is negative")"""
+    
+#fInd area of circle
+"""r=int(input("enter radius of circle"))
+
+# import math
+# p=math.pi
+pi=3.14
+#Const=float(3.14)
+Area=pi*r**2
+print("Area of circle is:", Area,".")"""
+
+#find simple iintres
+"""
+p=int(input("entr principal amount"))
+t=float(input("Enter the time period"))
+r=float(input("Entr the rate of interest"))
+SI=float(p*t*r)/100
+print ("Simple intrest of",p,"for",t,"year at",r," % rate is:",SI)"""
+
+import math
+print(math.sqrt(25))
+p=math.pi
+print(type(p))
